48bbb4daf4ee11ce29e979c83693c359bb220b4b
alvarodwi <varoa.dwi@gmail.com>
2026-05-16 20:58:09 +0700

    fix: add base url to lane cover url

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,7 +7,7 @@ const activeTab = ref<'randomizer' | 'config'>('randomizer')
 
 function openLaneCover() {
   window.open(
-    '/#/lane',
+    import.meta.env.BASE_URL + '#/lane',
     'lane-cover',
     'width=640,height=160'
   )
